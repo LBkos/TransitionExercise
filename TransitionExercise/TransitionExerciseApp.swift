99fb73065c9a161428e79c9dbaeb934ab7323abb
LBkos <constantin.l.o.p@gmail.com>
2023-07-11 15:54:33 +0700

    Initial Commit

---
+++ TransitionExercise/TransitionExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitionExerciseApp.swift
+//  TransitionExercise
+//
+//  Created by Константин Лопаткин on 11.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TransitionExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
